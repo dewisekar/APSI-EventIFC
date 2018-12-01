4737ebbfd61fb44cbc1a1bdd7098a611e861528b
dewisekar <sekar.6499@gmail.com>
2018-12-02 03:29:50 +0700

    punya dewi

--- routes/web.php
+++ routes/web.php
@@ -20,9 +20,16 @@ Route::group(['prefix' => 'admin'], function () {
     Route::get('dashboard', function () {
         return view('admin/admindashboard');
     })->name('adminhome');
+    Route::get('peserta', function () {
+        return view('admin/peserta');
+    });
+    Route::get('detailpeserta', function () {
+        return view('admin/detailpeserta');
+    });
 });
 
 Route::group(['prefix' => 'peminjaman'], function () {
     Route::get('', 'Peminjaman\ViewController@index');
     Route::get('/about_us', 'Peminjaman\ViewController@aboutUs');
 });
+
